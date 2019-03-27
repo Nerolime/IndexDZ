e54a30a544f0b8091b9be4f4e5914035618cf36f
Nerolime <46015793+Nerolime@users.noreply.github.com>
2019-03-27 10:06:34 +0600

    dz index

---
+++ Index/Indexator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index
+{
+    class Indexator
+    {
+        public int[] array=new int[10];
+        public int this[int i]
+        {
+            get
+            {
+
+                return array[i];
+            }
+            set
+            {
+                array[i] = value * value;
+            }
+           
+        }
+    }
+}
---
+++ Index/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexator indexator = new Indexator();
+            for (int i=0;i<indexator.array.Length;i++)
+            {
+                indexator[i] = i;
+                Console.WriteLine(indexator[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
